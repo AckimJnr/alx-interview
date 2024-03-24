25133de4bfc9ab9bd42904f95f9d170f57533209
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-24 21:44:13 +0200

    Add min ops file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+module: 0-minoperations
+"""
+
+
+def minOperations(n):
+    """
+    Calculates minmum # of operations to be done
+    to get n H chars
+    """
+    if n <= 0:
+        return 0
+
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        for j in range(2, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n] if dp[n] != float('inf') else 0
