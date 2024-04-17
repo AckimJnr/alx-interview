61828d59bd44fdf0d2fa5d2f22e463a5b977cd5d
AckimJnr <acklongwe@gmail.com>
2024-04-17 13:02:27 -0700

    Update semmistandard requirrement

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,43 @@
 #!/usr/bin/node
 const request = require('request');
-const host = "https://swapi-api.alx-tools.com/api/films/";
+const host = 'https://swapi-api.alx-tools.com/api/films/';
 
-if (process.argv.length < 3){
-    console.log("Usage: files MovieID");
-    return;
+if (process.argv.length < 3) {
+  console.log('Usage: files MovieID');
+  process.exit();
 }
-const movie_id = process.argv[2];
+const movieId = process.argv[2];
 
-const url = host + movie_id; 
+const url = host + movieId;
 
-request(url, function (error, response, body){
-    if (error) {
-        console.error("Error:", error);
-        return;
-    }
-    
-    if (response.statusCode !== 200) {
-        console.error("Status code:", response.statusCode);
-        return;
-    }
-    
-    const film = JSON.parse(body);
-    const characters = film.characters;
-    
-    characters.forEach(function(characterUrl) {
-        request(characterUrl, function(error, response, body) {
-            if (error) {
-                console.error("Error:", error);
-                return;
-            }
-            
-            if (response.statusCode !== 200) {
-                console.error("Status code:", response.statusCode);
-                return;
-            }
-            
-            const character = JSON.parse(body);
-            console.log(character.name);
-        });
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    process.exit();
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+    process.exit();
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach(function (characterUrl) {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
+        process.exit();
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Status code:', response.statusCode);
+        process.exit();
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
+  });
 });
