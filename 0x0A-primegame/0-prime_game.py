c7210d5ab36c41258da7474e8fbb479a9a428ab8
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-05-19 22:21:45 +0200

    Update prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,13 +8,17 @@ def isWinner(x, nums):
     Args:
         x (int): number of rounds
         nums (list): list of integers
+    Returns:
+        str: Name of the winner (Maria or Ben) or None if no winner.
     """
     if not nums or x < 1:
         return None
 
     n = max(nums)
 
-    prime = [True for _ in range(max(n + 1, 2))]
+    # Sieve of Eratosthenes
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False  # 0 and 1 are not primes
     p = 2
     while p * p <= n:
         if prime[p]:
@@ -22,23 +26,20 @@ def isWinner(x, nums):
                 prime[i] = False
         p += 1
 
-    prime[0] = prime[1] = False
-    c = 0
-    for i in range(len(prime)):
-        if prime[i]:
-            c += 1
-        prime[i] = c
+    # Cumulative count of primes up to each index
+    prime_count = [0] * (n + 1)
+    for i in range(1, n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if prime[i] else 0)
 
-    player = 0
-    for n in nums:
-        player += prime[n] % 2 == 1
+    maria_wins = 0
 
-    if player * 2 == len(nums):
-        return None
-    if player * 2 == 0:
-        return None
-    if player * 2 == len(nums):
-        return None
-    if player * 2 > len(nums):
+    for num in nums:
+        if prime_count[num] % 2 == 1:
+            maria_wins += 1
+
+    if maria_wins > x / 2:
         return "Maria"
-    return "Ben"
+    elif maria_wins < x / 2:
+        return "Ben"
+    else:
+        return None
