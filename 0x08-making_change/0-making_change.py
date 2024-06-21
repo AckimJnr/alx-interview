6ff4b2b155d00bde00e9dddff066e662c70e95c6
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-21 03:26:25 +0200

    Update file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,6 +17,8 @@ def makeChange(coins: int, total: int) -> int:
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
+    coins = sorted(coins, reverse=True)
+
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
