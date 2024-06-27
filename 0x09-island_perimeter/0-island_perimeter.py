d1b7f0d3de717b899f6937fc18d5b23317c2a176
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-06-27 09:34:59 +0200

    Add function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+island_perimeter module
+"""
+
+
+def island_perimeter(grid) -> int:
+    """
+    Calculate the perimeter of a grid
+    """
+    if not grid:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
